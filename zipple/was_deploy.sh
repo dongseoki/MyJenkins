3ccabb5527344760b203ac290ac2b88b107b05e7
dongseoki <43495319+dongseoki@users.noreply.github.com>
2024-08-07 23:11:37 +0900

    [fix] 이미지 삭제 로직 추가

--- zipple/was_deploy.sh
+++ zipple/was_deploy.sh
@@ -12,23 +12,7 @@ cd ${REPO_PATH}
 git fetch
 git pull
 
-echo 'remove none images'
-# <none>으로 태그된 이미지 ID를 가져옵니다.
-images_to_remove=$(docker images | grep '<none>' | awk '{print $3}')
 
-# 이미지 ID가 있는지 확인합니다.
-if [ -z "$images_to_remove" ]; then
-  echo "삭제할 이미지가 없습니다."
-  exit 0
-fi
-
-# 각 이미지 ID에 대해 삭제 명령을 실행합니다.
-for image_id in $images_to_remove; do
-  echo "이미지 삭제 중: $image_id"
-  docker rmi $image_id
-done
-
-echo "모든 <none> 이미지가 삭제되었습니다."
 
 
 echo 'build images'
@@ -48,4 +32,23 @@ docker compose up -d
 echo ">  ${SERVICE_NAME} dev deploy end"
 end_time=$(date +%s)
 elapsed_time=$((end_time - start_time))
+
+echo 'remove none images'
+# <none>으로 태그된 이미지 ID를 가져옵니다.
+images_to_remove=$(docker images | grep '<none>' | awk '{print $3}')
+
+# 이미지 ID가 있는지 확인합니다.
+if [ -z "$images_to_remove" ]; then
+  echo "삭제할 이미지가 없습니다."
+  exit 0
+fi
+
+# 각 이미지 ID에 대해 삭제 명령을 실행합니다.
+for image_id in $images_to_remove; do
+  echo "이미지 삭제 중: $image_id"
+  docker rmi $image_id
+done
+
+echo "모든 <none> 이미지가 삭제되었습니다."
+
 echo ">  ${SERVICE_NAME} Deployment took $elapsed_time seconds."
